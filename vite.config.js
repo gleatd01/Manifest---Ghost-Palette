c4697a6e7b6d92623a6b134e1b3c0ce6149a4d4d
gleatd01 <taylor@buzzedtop.com>
2026-07-02 15:07:03 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  define: {
+    // Maps the environment variable from Docker to a compile-time constant
+    __APP_VERSION__: JSON.stringify(process.env.GITHUB_REF_NAME || 'local-dev')
+  },
   server: {
     port: 5173,
     proxy: {
